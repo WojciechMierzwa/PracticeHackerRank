90ab5367ecb8e45825faee5a213bcdd1c8ec3072
WojciechMierzwa <96776516+WojciechMierzwa@users.noreply.github.com>
2022-10-26 20:03:51 +0200

    Create Arrays_and_Strings.c

---
+++ C/Arrays_and_Strings.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num, *arr, i, temp;
+    scanf("%d", &num);
+    arr = (int*) malloc(num * sizeof(int));
+    for(i = 0; i < num; i++) {
+        scanf("%d", arr + i);
+    }
+    
+
+    for(i = 0; i < num; i++)
+        printf("%d ", *(arr + num - 1- i));
+    return 0;
+}
